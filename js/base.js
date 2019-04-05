3a78328aa5214ad691d7da7938cd115ea28c3620
yinghualuowu <1048146365@qq.com>
2019-04-05 14:12:27 +0800

    再次调试

--- js/base.js
+++ js/base.js
@@ -856,14 +856,11 @@ function Base() {
             var html = '<div><ul>';
             obj.each(function (i) {
                 var name = $(obj[i]).prop("className");
-                var x = $(obj[i])[0].className;
-                console.log(name);
-                console.log(x);
-                if(name == 'recent_comment_title'){
+                if(typeof name == 'recent_comment_title'){
                     var o = $($(obj[i]).html());
                     html += '<li>' + o.prop("outerHTML") + '</li>';
                 }else{
-                    var o = $($(obj[i]).text());
+                    var o = $(obj[i]).prop("innerText");
                     html += '<li>' + o + '</li>';
                 }
             });
